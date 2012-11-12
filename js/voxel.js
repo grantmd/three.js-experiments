ac0dd5d926cec059ee03ab84da34fb86bbf15e39
Myles Grant <myles@mylesgrant.com>
2012-11-11 16:52:53 -0800

    Make sure the voxel positions are ints

--- js/voxel.js
+++ js/voxel.js
@@ -2,9 +2,9 @@ var voxelGeometry = new THREE.CubeGeometry(1, 1, 1);
 
 Voxel = function ( scene, position, color ) {
 	var voxel = new Physijs.BoxMesh(voxelGeometry, new THREE.MeshLambertMaterial({color: color}), 0, { friction: 0.8, restitution: 0.2 });
-	voxel.position.x = position.x;
-	voxel.position.y = position.y;
-	voxel.position.z = position.z;
+	voxel.position.x = parseInt(position.x, 10);
+	voxel.position.y = parseInt(position.y, 10);
+	voxel.position.z = parseInt(position.z, 10);
 
 	voxel.castShadow = true;
 	voxel.receiveShadow  = true;
