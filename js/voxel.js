cc389dab3ed973a038a9a4e479adb58788836f35
Myles Grant <myles@mylesgrant.com>
2012-11-11 16:38:32 -0800

    Just one copy of voxel geometry

--- js/voxel.js
+++ js/voxel.js
@@ -1,12 +1,14 @@
+var voxelGeometry = new THREE.CubeGeometry(1, 1, 1);
+
 Voxel = function ( scene, position, color ) {
-	var voxel = new Physijs.BoxMesh(new THREE.CubeGeometry(1, 1, 1), new THREE.MeshLambertMaterial({color: color}), 0, { friction: 0.8, restitution: 0.2 });
+	var voxel = new Physijs.BoxMesh(voxelGeometry, new THREE.MeshLambertMaterial({color: color}), 0, { friction: 0.8, restitution: 0.2 });
 	voxel.position.x = position.x;
 	voxel.position.y = position.y;
 	voxel.position.z = position.z;
 
 	voxel.castShadow = true;
 	voxel.receiveShadow  = true;
-	
+
 	scene.add(voxel);
 };
 
